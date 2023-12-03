2cc491f5098d6b1b049fb7b084be1d07a83fc3e9
Vladislav <hohsens@gmail.com>
2023-12-03 14:35:26 +0200

    added routes for Cars, Champions and News

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\ChampionController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Cars routes
+Route::get('cars', [CarController::class, 'index'])->name('car.index');
+Route::get('cars/create', [CarController::class, 'create']);
+Route::post('cars', [CarController::class, 'store'])->name('car.store');
+Route::get('cars/{id}', [CarController::class, 'edit'])->name('car.edit');
+Route::put('cars/{id}', [CarController::class, 'update'])->name('car.update');
+Route::delete('cars/{id}', [CarController::class, 'destroy'])->name('car.delete');
+
+//Champions routes
+Route::get('champions', [ChampionController::class, 'index'])->name('champion.index');
+Route::get('champions/create', [ChampionController::class, 'create']);
+Route::post('champions', [ChampionController::class, 'store'])->name('champion.store');
+Route::get('champions/{id}', [ChampionController::class, 'edit'])->name('champion.edit');
+Route::put('champions/{id}', [ChampionController::class, 'update'])->name('champion.update');
+Route::delete('champions/{id}', [ChampionController::class, 'destroy'])->name('champion.delete');
+
+//News routes
+Route::get('news', [NewsController::class, 'index'])->name('news.index');
+Route::get('news/create', [NewsController::class, 'create']);
+Route::post('news', [NewsController::class, 'store'])->name('news.store');
+Route::get('news/{id}', [NewsController::class, 'edit'])->name('news.edit');
+Route::put('news/{id}', [NewsController::class, 'update'])->name('news.update');
+Route::delete('news/{id}', [NewsController::class, 'destroy'])->name('news.delete');
